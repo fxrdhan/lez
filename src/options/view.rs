a2517c0e4825e3ab79fe790b67c06bed0ae6d159
Firdaus Arif R <firdausarief65@gmail.com>
2026-08-21 05:59:58 +0700

    feat(time): accept 'r', 'm', and 'mod' as aliases for --time modified (eza#1775)

--- src/options/view.rs
+++ src/options/view.rs
@@ -705,6 +705,72 @@ mod tests {
     }
 
     #[test]
+    fn deduce_time_types_modified_word_mod() {
+        assert_eq!(
+            TimeTypes::deduce(&mock_cli(vec!["--time=mod"])),
+            Ok(TimeTypes {
+                modified: true,
+                ..TimeTypes::default()
+            })
+        );
+    }
+
+    #[test]
+    fn deduce_time_types_modified_word_m() {
+        assert_eq!(
+            TimeTypes::deduce(&mock_cli(vec!["--time=m"])),
+            Ok(TimeTypes {
+                modified: true,
+                ..TimeTypes::default()
+            })
+        );
+    }
+
+    #[test]
+    fn deduce_time_types_modified_word_r() {
+        assert_eq!(
+            TimeTypes::deduce(&mock_cli(vec!["--time=r"])),
+            Ok(TimeTypes {
+                modified: true,
+                ..TimeTypes::default()
+            })
+        );
+    }
+
+    #[test]
+    fn deduce_time_types_short_time_r() {
+        assert_eq!(
+            TimeTypes::deduce(&mock_cli(vec!["-t=r"])),
+            Ok(TimeTypes {
+                modified: true,
+                ..TimeTypes::default()
+            })
+        );
+    }
+
+    #[test]
+    fn deduce_time_types_short_time_m() {
+        assert_eq!(
+            TimeTypes::deduce(&mock_cli(vec!["-t=m"])),
+            Ok(TimeTypes {
+                modified: true,
+                ..TimeTypes::default()
+            })
+        );
+    }
+
+    #[test]
+    fn deduce_time_types_short_time_mod() {
+        assert_eq!(
+            TimeTypes::deduce(&mock_cli(vec!["-t=mod"])),
+            Ok(TimeTypes {
+                modified: true,
+                ..TimeTypes::default()
+            })
+        );
+    }
+
+    #[test]
     fn deduce_time_types_accessed_word() {
         assert_eq!(
             TimeTypes::deduce(&mock_cli(vec!["--time=accessed"])),
