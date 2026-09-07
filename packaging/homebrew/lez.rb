f3eae37bb3c43b39e02b1a336b375c63f8f163ca
Firdaus Arif R <firdausarief65@gmail.com>
2026-09-07 13:12:16 +0700

    ci(release): automate Homebrew tap formula updates on release

--- packaging/homebrew/lez.rb
+++ packaging/homebrew/lez.rb
@@ -5,6 +5,7 @@ class Lez < Formula
   desc "A modern, fast, and feature-rich replacement for ls written in Rust"
   homepage "https://github.com/fxrdhan/lez"
   url "https://github.com/fxrdhan/lez/archive/refs/tags/v0.28.3.tar.gz"
+  sha256 "c6af809d2f8bd7adf50c7fc5fa14727aa209e0aa024952a0bf3f856dfededf18"
   license "EUPL-1.2"
   head "https://github.com/fxrdhan/lez.git", branch: "main"
 
